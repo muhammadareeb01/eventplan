2ebd95110bc769087d7efa39228b8259b73af5e5
muhammadareeb01 <muhammadareebali795@gmail.com>
2026-01-29 03:04:52 +0500

    'updated'

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxpUsXBBhzPoZ-RFHfghsFyoopVLoYHos",
-  authDomain: "eventplan-47374.firebaseapp.com",
-  projectId: "eventplan-47374",
-  storageBucket: "eventplan-47374.firebasestorage.app",
-  messagingSenderId: "678373205697",
-  appId: "1:678373205697:web:ee907b4833cefc1a03d640",
-  measurementId: "G-CGGR3RF3Y5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
